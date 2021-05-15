72016c852db8964b9e1cfbbfa24810a282dd029b
Luan <Luan_Gashi@sluz.ch>
2021-05-15 12:39:09 +0200

    Setup Project

---
+++ M151_Lager/M151_Lager/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M151_Lager.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
